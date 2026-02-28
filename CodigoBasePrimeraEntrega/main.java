1a1a3f841a9ac29b767533c1ca536409f68f36d1
jdcc22 <142845607+jdcc22@users.noreply.github.com>
2026-02-28 18:21:31 -0500

    Created main.java to add the code folder

---
+++ CodigoBasePrimeraEntrega/main.java
@@ -0,0 +1,37 @@
+public class Main
+{
+	public static void main(String[] args) 
+	{
+		Test();
+	}
+	
+	public static void Test() 
+	{
+	    CentralArray arr = new CentralArray();
+	    
+	    arr.InsertNewStudent(1, "A");
+	    arr.InsertNewStudent(2, "B");
+	    arr.InsertNewStudent(10, "D");
+	    arr.InsertNewStudent(4, "C");
+	    arr.InsertNewStudent(81, "E");
+	    arr.InsertNewStudent(9000000, "F");
+	    
+	    arr.PrintList();
+	    
+	    arr.RemoveStudentByIndex(4);
+	    
+	    arr.PrintList();
+
+	    Estudiante es = new Estudiante(1, "A");
+	    
+	    arr.RemoveStudent(es);
+	    
+	    arr.PrintList();
+	    
+	    arr.InsertNewStudent(7, "A");
+	    
+	    arr.PrintList();
+	    
+	    
+	}
+}
