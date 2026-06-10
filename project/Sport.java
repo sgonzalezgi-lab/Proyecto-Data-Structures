f02bf7ce7a00c4cc87d003ded1d718d007221406
jdcc22 <142845607+jdcc22@users.noreply.github.com>
2026-06-09 19:41:54 -0500

    Updated Sport.java

--- project/Sport.java
+++ project/Sport.java
@@ -1,33 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.projectdatastructure;
+
 public class Sport implements Comparable<Sport> {
-    //attributes
+ 
     String name;
     int amountStu;
     DoublyLinkedList<Student> practicers;
+    boolean visited;
  
-    
-    
     Sport(String name) {
         this.name = name;
         this.amountStu = 0;
         this.practicers = new DoublyLinkedList<>();
+        this.visited = false;
     }
  
-    
     public DobleNode<Student> addPracticer(Student student) {
+        //save the reference of the student
+        //to make more efficient the removing of student
         DobleNode<Student> ref = practicers.pushFront(student);
         amountStu++;
-        //return the reference to save in the dinamicArray of references in the student
         return ref;
     }
  
-    
     public void removePracticerByRef(DobleNode<Student> ref) {
-        //to remove the students of sports when a student is removed
+        //take advantage of the reference to remove the student
         practicers.removeByReference(ref);
         amountStu--;
     }
  
     public void removePracticer(Student student) {
+        //this is used when we wanna remove a student only from a sport
         practicers.remove(student);
         amountStu--;
     }
@@ -38,7 +44,7 @@ public class Sport implements Comparable<Sport> {
         practicers.print();
     }
  
- 
+    //they are ordered by name
     @Override
     public int compareTo(Sport other) {
         return this.name.compareTo(other.name);
